45267873ec159f0ee74c76b710ec4f9ea8e53dfd
Muhammad Ridhwan <107637984+IshqhsI@users.noreply.github.com>
2022-11-07 09:56:09 +0700

    Initial Commit

---
+++ Form1.Designer.vb
@@ -0,0 +1,175 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(436, 151)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(57, 17)
+        Me.Label1.TabIndex = 12
+        Me.Label1.Text = "HARGA"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"5%", "10%", "15%"})
+        Me.ComboBox1.Location = New System.Drawing.Point(720, 304)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(307, 24)
+        Me.ComboBox1.TabIndex = 23
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(433, 203)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 17)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "BANYAK"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(436, 253)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(117, 17)
+        Me.Label3.TabIndex = 14
+        Me.Label3.Text = "BANYAK*HARGA"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(436, 304)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(60, 17)
+        Me.Label4.TabIndex = 15
+        Me.Label4.Text = "DISKON"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(436, 354)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(110, 17)
+        Me.Label5.TabIndex = 16
+        Me.Label5.Text = "TOTAL DISKON"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(436, 409)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(242, 17)
+        Me.Label6.TabIndex = 17
+        Me.Label6.Text = "(BANYAK*HARGA) - TOTAL DISKON"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(720, 151)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(307, 22)
+        Me.TextBox1.TabIndex = 18
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(720, 203)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(307, 22)
+        Me.TextBox2.TabIndex = 19
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(720, 253)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(307, 22)
+        Me.TextBox3.TabIndex = 20
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(720, 354)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.ReadOnly = True
+        Me.TextBox4.Size = New System.Drawing.Size(307, 22)
+        Me.TextBox4.TabIndex = 21
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(720, 409)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.ReadOnly = True
+        Me.TextBox5.Size = New System.Drawing.Size(307, 22)
+        Me.TextBox5.TabIndex = 22
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1554, 728)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox5 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,44 @@
+﻿Public Class Form1
+
+
+   
+
+    Private Sub ComboBox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox1.SelectedIndexChanged
+        Dim harga, banyak, total, total_setdiskon, diskonjum As Integer
+        Dim diskon As String
+
+        harga = Val(TextBox1.Text)
+        banyak = Val(TextBox2.Text)
+        total = Val(TextBox3.Text)
+        diskon = ComboBox1.Text
+
+        If diskon = "5%" Then
+            total = harga * banyak
+            diskonjum = total * 5 / 100
+            total_setdiskon = total - diskonjum
+        ElseIf diskon = "10%" Then
+            total = harga * banyak
+            diskonjum = total * 10 / 100
+            total_setdiskon = total - diskonjum
+        ElseIf diskon = "15%" Then
+            total = harga * banyak
+            diskonjum = total * 15 / 100
+            total_setdiskon = total - diskonjum
+        Else
+            total = harga * banyak
+            diskonjum = "diskon tidak dipilih"
+            total_setdiskon = "diskon tidak dipilih"
+        End If
+
+        TextBox4.Text = "Rp. " + Str(diskonjum) + " "
+        TextBox5.Text = "Rp. " + Str(total_setdiskon)
+    End Sub
+
+    Private Sub TextBox2_Keyup(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.KeyUp
+        Dim harga, banyak As Integer
+
+        harga = Val(TextBox1.Text)
+        banyak = Val(TextBox2.Text)
+        TextBox3.Text = "Rp. " + Str(harga * banyak)
+    End Sub
+End Class
